3fde4e39206eeb95f541efb7ad72ef78ad97630e
Miguel Jimenez <migang9@gmail.com>
2021-06-23 15:30:56 -0400

    implement CountLetters

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return;
+};
+
+const countLetters = function(string) {
+  let letters = string.split(' ').join('');
+  let result = {};
+  for (let letter of letters) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("hello world"));
