ada88fb91ea5d536ce1150ff2fc274b6f25027a3
Miguel Jimenez <migang9@gmail.com>
2021-06-28 15:18:32 -0400

    open test files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+// test
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(3, 9);
+assertEqual("Hi", "hi");
+assertEqual("Hi", "Hi");
+assertEqual(3, "3");
---
+++ test/headTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+
+// test cases:
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 6);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
